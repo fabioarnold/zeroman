30864b749b355a2cf7c3f1fc8f92f5455bca1943
Fabio Arnold <fabio@fabioarnold.de>
2023-01-26 00:23:06 +0100

    Set `rdynamic` flag to export symbols

--- build.zig
+++ build.zig
@@ -8,6 +8,7 @@ pub fn build(b: *std.build.Builder) void {
     wasm.setBuildMode(mode);
     wasm.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
     wasm.install();
+    wasm.rdynamic = true;
     b.step("wasm", "build/install the wasm file").dependOn(&wasm.install_step.?.step);
 
     {
