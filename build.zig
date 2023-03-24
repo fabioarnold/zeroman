0b38e12a0e361637453922ad8d62c98da75e0a5d
Fabio Arnold <fabio@fabioarnold.de>
2023-03-24 17:01:11 +0100

    Make steps depend on each other to prevent race

--- build.zig
+++ build.zig
@@ -12,16 +12,12 @@ pub fn build(b: *std.Build) void {
     });
     wasm.rdynamic = true;
     wasm.install();
-    b.step("wasm", "build/install the wasm file").dependOn(&wasm.install_step.?.step);
 
-    {
-        const process_assets = ProcessAssetsStep.create(b);
-        process_assets.addStage("maps/stages/needleman/needleman.world");
-        wasm.step.dependOn(&process_assets.step);
-    }
-    {
-        // format generated files
-        const fmt = b.addFmt(.{ .paths = &.{"src/stages/needleman.zig"} });
-        wasm.step.dependOn(&fmt.step);
-    }
+    const process_assets = ProcessAssetsStep.create(b);
+    process_assets.addStage("maps/stages/needleman/needleman.world");
+
+    // format generated files
+    const fmt = b.addFmt(.{ .paths = &.{"src/stages/needleman.zig"} });
+    fmt.step.dependOn(&process_assets.step);
+    wasm.step.dependOn(&fmt.step);
 }
