789d1747cd2464956081aa7dadc900daad933557
Fabio Arnold <fabio@fabioarnold.de>
2023-12-08 00:10:06 +0100

    Shoot enemies

--- src/Enemy.zig
+++ src/Enemy.zig
@@ -12,18 +12,22 @@ pub const Type = enum {
     gopher,
 };
 
-pub var gopher_sprite: Renderer.Texture = undefined;
+var gopher_sprite: Renderer.Texture = undefined;
+var hurt_fx: Renderer.Texture = undefined;
 
 active: bool = false,
 type: Type = undefined,
 box: Box = undefined,
 state: u8 = 0,
+health: u8 = 0,
+invincibility_frames: u8 = 0,
 frame: u8 = 0,
 counter: i32 = 0,
-flip_x: bool = false,
+face_left: bool = false,
 
 pub fn load() void {
     gopher_sprite.loadFromUrl("img/gopher.png", 72, 24);
+    hurt_fx.loadFromUrl("img/hurt.png", 24, 24);
 }
 
 pub fn activate(self: *Self, @"type": Type, box: Box) void {
@@ -31,7 +35,9 @@ pub fn activate(self: *Self, @"type": Type, box: Box) void {
     self.type = @"type";
     self.box = box;
     switch (@"type") {
-        .gopher => {},
+        .gopher => {
+            self.health = 2;
+        },
     }
     self.state = 0;
     self.frame = 0;
@@ -41,6 +47,16 @@ pub fn activate(self: *Self, @"type": Type, box: Box) void {
 pub fn tick(self: *Self, r: std.rand.Random, game: *GameData, attribs: []const Attrib) void {
     switch (self.type) {
         .gopher => tickGopher(self, r, game, attribs),
+    }
+}
+
+pub fn hurt(self: *Self, damage: u8) void {
+    if (self.invincibility_frames > 0) return;
+
+    self.health -|= damage;
+    self.invincibility_frames = 30;
+    if (self.health == 0) {
+        self.active = false;
     }
 }
 
@@ -58,10 +74,12 @@ const GopherState = enum(u8) {
 fn tickGopher(self: *Self, r: std.rand.Random, game: *GameData, attribs: []const Attrib) void {
     const room = game.getCurrentRoom();
     const state: *GopherState = @ptrCast(&self.state);
+    self.invincibility_frames -|= 1;
+    if (self.invincibility_frames > 0) return;
     switch (state.*) {
         .idle => {
             self.frame = 0;
-            self.flip_x = self.counter & 16 != 0;
+            self.face_left = self.counter & 16 != 0;
             if (self.counter <= 0) {
                 self.counter = r.intRangeLessThan(i32, 100, 500);
                 state.* = .walk;
@@ -69,10 +87,10 @@ fn tickGopher(self: *Self, r: std.rand.Random, game: *GameData, attribs: []const
         },
         .walk => {
             self.frame = if (self.counter & 8 != 0) 1 else 2;
-            const amount: i32 = if (self.flip_x) -1 else 1;
+            const amount: i32 = if (self.face_left) -1 else 1;
             const sense_x = self.box.x + @divTrunc(self.box.w + amount * self.box.w, 2);
             if (room.getTileAttribAtPixel(attribs, sense_x, self.box.y + self.box.h) != .solid or room.clipX(attribs, self.box, amount) != amount) {
-                self.flip_x = !self.flip_x;
+                self.face_left = !self.face_left;
             } else {
                 self.box.x += amount;
             }
@@ -90,9 +108,14 @@ fn tickGopher(self: *Self, r: std.rand.Random, game: *GameData, attribs: []const
 }
 
 fn drawGopher(self: Self) void {
+    if (self.invincibility_frames % 6 >= 3) {
+        Renderer.Sprite.draw(hurt_fx, self.box.x - 4, self.box.y);
+        return;
+    }
+
     var src_rect = Rect.init(self.frame * 24, 0, 24, 24);
     const dst_rect = Rect.init(self.box.x - 4, self.box.y, 24, 24);
-    if (self.flip_x) {
+    if (self.face_left) {
         src_rect.x += src_rect.w;
         src_rect.w = -src_rect.w;
     }
